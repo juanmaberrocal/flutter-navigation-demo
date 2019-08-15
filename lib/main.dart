060c89d6788b470a43c604d8a392ff92905d7fcb
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-08-15 10:45:48 -0400

    Return data from a screen

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,36 @@ class NavigationButton extends StatelessWidget {
   }
 }
 
+class SelectionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {
+        _navigateAndDisplaySelection(context);
+      },
+      child: Text('Pick an option, any option!'),
+    );
+  }
+
+  // A method that launches the SelectionScreen and awaits the
+  // result from Navigator.pop.
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    final result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step.
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+
+    // After the Selection Screen returns a result, hide any previous snackbars
+    // and show the new result.
+    Scaffold.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text("$result")));
+  }
+}
+
 class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -83,21 +113,27 @@ class FirstRoute extends StatelessWidget {
         title: Text('First Route'),
       ),
       body: Center(
-        /* child: RaisedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-            Navigator.pushNamed(context, '/second');
-          },
-        ), */
-        child: NavigationButton(
-          buttonText: 'Custom navigation button text',
-          routeName: ExtractArgumentsScreen.routeName,
-          arguments: ScreenArguments(
-            'Extract Arguments Screen',
-            'This message is extracted in the build method. Hot reload',
-          ),
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text('Open route'),
+              onPressed: () {
+                // Navigate to second route when tapped.
+                Navigator.pushNamed(context, '/second');
+              },
+            ),
+            NavigationButton(
+              buttonText: 'Custom navigation button text',
+              routeName: ExtractArgumentsScreen.routeName,
+              arguments: ScreenArguments(
+                'Extract Arguments Screen',
+                'This message is extracted in the build method. Hot reload',
+              ),
+            ),
+            SelectionButton(),
+          ]
+        )
       ),
     );
   }
@@ -139,6 +175,44 @@ class ExtractArgumentsScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(args.message),
+      ),
+    );
+  }
+}
+
+class SelectionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pick an option'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Pop here with "Yep"...
+                  Navigator.pop(context, 'Yep!');
+                },
+                child: Text('Yep!'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Pop here with "Nope"
+                  Navigator.pop(context, 'Nope!');
+                },
+                child: Text('Nope.'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
